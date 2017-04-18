80fb35cba5b793881764ea042f99744c40ff9672
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-04-18 17:44:34 +1000

    I am...bananagramr

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Counts of letters from Banangrams.
+#'
+#' Counts of the letters from two randomly sorted bags of bananagrams.
+#'
+#' @format A data frame with three variables: `letter`, `bag_1`, and `bag_2`
+#'
+#' \describe{
+#' \item{`letter`}{letters from A-Z}
+#' \item{`bag_1`}{Number of each letter in bag 1}
+#' \item{`bag_2`}{Number of each letter in bag 2}
+#' }
+#'
+"dat_bananagram"
---
+++ README.md
@@ -0,0 +1,11 @@
+# bananagramr
+
+The goal of bananagramr is to ...
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+## basic example code
+```
